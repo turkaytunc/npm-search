ad45e19cb4c01a3b66cc17d30136db57df9f47c9
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-01 10:43:20 +0300

    add: actions and action types

--- src/redux/reducers/repoReducer.ts
+++ src/redux/reducers/repoReducer.ts
@@ -3,13 +3,34 @@ interface RepoState {
   error: string | null;
   data: string[];
 }
-const repoReducer = (state: RepoState, action: any) => {
+
+enum ActionType {
+  ERROR = 'error',
+  SUCCESS = 'success',
+  SEARCH = 'search',
+}
+
+type RepoAction =
+  | {
+      type: ActionType.ERROR;
+      payload: string;
+    }
+  | {
+      type: ActionType.SUCCESS;
+      payload: string[];
+    }
+  | {
+      type: ActionType.SEARCH;
+      payload: null;
+    };
+
+const repoReducer = (state: RepoState, action: RepoAction): RepoState => {
   switch (action.type) {
-    case 'search':
+    case ActionType.SEARCH:
       return { loading: true, error: null, data: [] };
-    case 'success':
+    case ActionType.SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case 'error':
+    case ActionType.ERROR:
       return { loading: false, error: action.payload, data: [] };
     default:
       return state;
